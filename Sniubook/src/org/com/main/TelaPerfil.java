e8c3bf80ba9111e7ccc26c5ef10c4fe4415f0d0b
mathferr <mathferr@users.noreply.github.com>
2015-11-20 19:04:20 -0200

    Ajustes TelaPerfil.java

--- Sniubook/src/org/com/main/TelaPerfil.java
+++ Sniubook/src/org/com/main/TelaPerfil.java
@@ -13,13 +13,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class TelaPerfil extends Activity {
 	
 	SQLiteDatabase BancoDados;
-	Button btDeslogar;
-	TextView tvRegistroAcademico, tvNome, tvCPF, tvCurso, tvPeriodo, tvCampus, tvTurma;
+	ImageView imageTelaPerfil;
+	Button btDeslogar, btPerfilAlterarSenha;
+	TextView tvPerfilRegistroAcademico, tvPerfilCPF, tvPerfilTurma;
+	EditText txtPerfilNome;
+	Spinner spPerfilCurso, spPerfilPeriodo, spPerfilCampus, spPerfilTurno;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,16 @@ public class TelaPerfil extends Activity {
 				Intent telaAnterior = new Intent(TelaPerfil.this, TelaPrincipal.class);
 				TelaPerfil.this.startActivity(telaAnterior);
 				TelaPerfil.this.finish();
+			}
+			
+		});
+		
+		btPerfilAlterarSenha.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
 			}
 		});
 	}
@@ -60,13 +76,16 @@ public class TelaPerfil extends Activity {
 	
 	public void inicializarTelaPerfil() {
 		btDeslogar = (Button) findViewById(R.id.btDeslogar);
-		tvRegistroAcademico = (TextView) findViewById(R.id.tvRegistroAcademico);
-		tvNome = (TextView) findViewById(R.id.tvNome);
-		tvCPF = (TextView) findViewById(R.id.tvCPF);
-		tvCurso = (TextView) findViewById(R.id.tvCurso);
-		tvPeriodo = (TextView) findViewById(R.id.tvPeriodo);
-		tvCampus = (TextView) findViewById(R.id.tvCampus);
-		tvTurma = (TextView) findViewById(R.id.tvTurma);
+		tvPerfilRegistroAcademico = (TextView) findViewById(R.id.tvPerfilRegistroAcademico);
+		txtPerfilNome = (EditText) findViewById(R.id.txtPerfilNome);
+		tvPerfilCPF = (TextView) findViewById(R.id.tvPerfilCPF);
+		tvPerfilTurma = (TextView) findViewById(R.id.tvPerfilTurma);
+		spPerfilCurso = (Spinner) findViewById(R.id.spPerfilCurso);
+		spPerfilPeriodo = (Spinner) findViewById(R.id.spPerfilPeriodo);
+		spPerfilCampus = (Spinner) findViewById(R.id.spPerfilCampus);
+		spPerfilTurno = (Spinner) findViewById(R.id.spPerfilTurno);
+		imageTelaPerfil = (ImageView) findViewById(R.id.imageTelaPerfil);
+		btPerfilAlterarSenha = (Button) findViewById(R.id.btPerfilAlterarSenha);
 	}
 	
 	@Override
@@ -76,32 +95,55 @@ public class TelaPerfil extends Activity {
 		Aluno perfil = TelaMainActivity.perfil;
 		try {
 			BancoDados = openOrCreateDatabase("sniubook", MODE_WORLD_READABLE, null);
-			String sql = "SELECT c.nome, cp.nome FROM curso c, campus cp WHERE c.codigo = '" + perfil.getCurso()
+			String sql = "SELECT c.codigo, cp.codigo FROM curso c, campus cp WHERE c.codigo = '" + perfil.getCurso()
 			+ "' AND cp.codigo = '" + perfil.getCampus() + "'";
 			Cursor cursor = BancoDados.rawQuery(sql, null);
 			if (cursor.getCount() > 0) {
 				cursor.moveToFirst();
 				curso = cursor.getString(0);
 				campus = cursor.getString(1);
-				tvCurso.setText(curso);
-				tvCampus.setText(campus);
+				for (int i = 0; i < spPerfilCurso.getCount(); i++) {
+					if (spPerfilCurso.getItemAtPosition(i).toString().startsWith(curso)) {
+						spPerfilCurso.setSelection(i);
+					}
+				}
+				for (int i = 0; i < spPerfilCampus.getCount(); i++) {
+					if (spPerfilCampus.getItemAtPosition(i).toString().startsWith(campus)) {
+						spPerfilCampus.setSelection(i);
+					}
+				}
 			} else {
 				exibirMensagem("Erro", "Erro ao preencher curso e campus.");
 			}
+			
+			sql = "SELECT imagem FROM ";
 		} catch (Exception erro) {
 			exibirMensagem("Erro", "Ocorreu um erro no banco de dados.\n" + erro.toString());
 		} finally {
 			BancoDados.close();
 		}
 		
-		tvRegistroAcademico.setText(perfil.getRegistroAcademico() + "");
-		if (perfil.getRegistroAcademico() == 0) tvRegistroAcademico.setText("(Ex-Aluno)");
-		tvNome.setText(perfil.getNome() + "");
-	    tvCPF.setText(perfil.getCpf() + "");
-		tvPeriodo.setText(perfil.getPeriodo() + "");
-		if (perfil.getPeriodo() == null) tvPeriodo.setText("(Ex-Aluno)");
-		tvTurma.setText(perfil.getTurma());
-		if (perfil.getTurma() == null) tvTurma.setText("(Ex-Aluno)");
+		tvPerfilRegistroAcademico.setText(perfil.getRegistroAcademico() + "");
+		if (perfil.getRegistroAcademico() == 0) tvPerfilRegistroAcademico.setText("(Ex-Aluno)");
+		txtPerfilNome.setText(perfil.getNome() + "");
+		tvPerfilCPF.setText(perfil.getCpf() + "");
+		if (perfil.getPeriodo() != null) {
+			spPerfilPeriodo.setEnabled(true);
+			for (int i = 0; i < spPerfilPeriodo.getCount(); i++) {
+				if (spPerfilPeriodo.getItemAtPosition(i).toString().contains(perfil.getPeriodo())) {
+					spPerfilPeriodo.setSelection(i);
+				}
+			}
+		} else {
+			spPerfilPeriodo.setEnabled(false);
+		}
+		for (int i = 0; i < spPerfilTurno.getCount(); i++) {
+			if (spPerfilTurno.getItemAtPosition(i).toString().toLowerCase().startsWith(perfil.getTurno().toLowerCase())) {
+				spPerfilTurno.setSelection(i);
+			}
+		}
+		tvPerfilTurma.setText(perfil.getTurma());
+		if (perfil.getTurma() == null) tvPerfilTurma.setText("(Ex-Aluno)");
 	}
 	
 	public void exibirMensagem(String tituloMensagem, String mensagem) {
